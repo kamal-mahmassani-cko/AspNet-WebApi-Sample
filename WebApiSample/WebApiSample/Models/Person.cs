d7dafcbdba5dd5ba88f0f267b46b7fbc93c1396a
Kamal Mahmassani <66256462+kamal-mahmassani-cko@users.noreply.github.com>
2025-05-08 10:59:36 +0100

    Update WebApiSample/WebApiSample/Models/Person.cs

--- WebApiSample/WebApiSample/Models/Person.cs
+++ WebApiSample/WebApiSample/Models/Person.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+         [Key]
+         public string Phone { get; set; }
+         public string Name { get; set; }
+        public string Address { get; set; }
+     }
+ }
 
-namespace WebApiSample.Models
-{
-    public class Person
-    {
-        [Key]
-        public string Phone { get; set; }
-        public string Name { get; set; }
-    }
-}
+
